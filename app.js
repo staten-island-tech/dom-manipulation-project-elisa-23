15d308c7e527b3400f3785c5461e4a757485cda7
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-10-10 19:53:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 DOMSelectors = {
-    cards: document.querySelectorAll(".card"),
     form: document.querySelector("form"),
     input1: document.querySelector("#input1"),
     input2: document.querySelector("#input2"),
     input3: document.querySelector("#input3"),
     container: document.querySelector(".container"),
-    removeBtns: document.querySelectorAll(".remove-btn"),
 };
 
 function createCard() {
     DOMSelectors.form.addEventListener("submit", function(event) {
-        let amountOfCards = [];
-        DOMSelectors.cards.forEach((card)=> {
-            if (!amountOfCards.includes(card.getAttribute('id'))) {
-                amountOfCards.push(card.getAttribute('id'));
-            }
-        });
+        let cardCount = document.querySelectorAll(".card").length;
         console.log("Form submitted");
         event.preventDefault();
         console.log("Default Action Prevented");
         let inputs = [DOMSelectors.input1.value, DOMSelectors.input2.value, DOMSelectors.input3.value];     /* text.value ==> gets the value inputted by the user */
         console.log(inputs);
-        let id;
-        if (amountOfCards.length === 0) {
-            id = 1;
-        } else {
-            id = amountOfCards.length + 1;
-        }
+        let id = cardCount + 1;
         DOMSelectors.container.insertAdjacentHTML("beforeend", 
             `<div class="card">
             <h2 class="card-header" id="card${id}">${inputs[0]}</h2>
@@ -43,14 +31,28 @@ function createCard() {
 createCard();
 
 function removeCard() {
-    removeBtns = Array.from(DOMSelectors.removeBtns);
-    removeBtns.forEach(addEventListener("click", function(event) {
-        const id = event.target.getAttribute('id');
-        const cardID = "card" + id;
-        let card = document.getElementById(cardID);
-        card.remove();
-        console.log("Card#" + cardID + "removed");
-    }));
+    DOMSelectors.container.addEventListener("click", function(event){
+        const cards = document.querySelectorAll(".card");
+        const allRemoveBtns = document.querySelectorAll(".remove-btn");
+        const removeBtns = Array.from(allRemoveBtns);
+        console.log(removeBtns);
+        removeBtns.forEach((button)=>button.addEventListener("click", function(event) {
+            const id = button.getAttribute('id');
+            const cardID = "card" + id;
+            console.log(cardID);
+            let arrayOfCards=[];
+            if (!arrayOfCards.includes(cards.getAttribute('id'))) {
+                arrayOfCards.push(cards.getAttribute('id'));
+            }
+            arrayOfCards.forEach((card) => { if (card === cardID) {
+                if ((arrayOfCards.length) !== -1) {
+                    const removeEle = document.getElementById(card);
+                    removeEle.html.remove();
+                    console.log("Card Removed");
+                }
+        }});
+        }));
+    });
 }
 
-removeCard()
+removeCard();
